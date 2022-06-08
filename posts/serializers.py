7cc46e7836dcf826ab59781f57ce38d19220e685
Christine Greaney-Kelley <kelleyc416@gmail.com>
2022-06-08 15:29:08 +0000

    Add image filter and set required image size

--- posts/serializers.py
+++ posts/serializers.py
@@ -12,9 +12,25 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 *2:
+            raise serializers.ValidationError(
+                'Image size larger than 2MB!'
+        )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px'
+        )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px'
+        )
+        return value
+
     class Meta:
         model = Post
         fields = [
                  'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
-                 'created_at', 'updated_at', 'title', 'content', 'image'
+                 'created_at', 'updated_at', 'title', 'content', 'image',
+                 'image_filter'
         ]
