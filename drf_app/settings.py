1391de4d8990d42653337dbf2051da3b30937330
Christine Greaney-Kelley <kelleyc416@gmail.com>
2022-06-10 16:19:51 +0000

    Fix formatting in Rest_framework variable

--- drf_app/settings.py
+++ drf_app/settings.py
@@ -67,7 +67,10 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication' 
         if 'DEV' in os.environ 
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    )]
+    )], 
+    'DEFAULT_PAGINATION_CLASS':  'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DATETIME_FORMAT': '%d %b %Y'
     }
 
 REST_USE_JWT = True
@@ -78,21 +81,10 @@ JWT_AUTH_SECURE = True
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 REST_AUTH_SERIALIZERS = {'USER_DETAILS_SERIALIZER': 'drf_app.serializers.CurrentUserSerializer'}
 
-REST_FRAMEWORK = {
-    ...,
-    'DEFAULT_PAGINATION_CLASS':  'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
-    }
-
 if 'DEV' not in os.environ:
     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
         'restframework.renderers.JSONRenderer'
     ]
-
-REST_FRAMEWORK = {
-   ...
-    'DATETIME_FORMAT': '%d %b %Y'
-    }
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
