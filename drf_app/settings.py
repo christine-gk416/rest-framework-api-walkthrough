ae1c7b77188f6d99e78b6490ad5dbad2e147bdc4
Christine Greaney-Kelley <kelleyc416@gmail.com>
2022-06-26 16:42:59 +0000

    Set debug to false

--- drf_app/settings.py
+++ drf_app/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEV' in os.environ
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['api-drf-moments.herokuapp.com','localhost']
 
@@ -66,29 +66,29 @@ INSTALLED_APPS = [
 
 SITE_ID = 2
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': [( 
-#         'rest_framework.authentication.SessionAuthentication' 
-#         if 'DEV' in os.environ  
-#         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-#     )], 
-#     'DEFAULT_PAGINATION_CLASS':  'rest_framework.pagination.PageNumberPagination',
-#     'PAGE_SIZE': 10,
-#     'DATETIME_FORMAT': '%d %b %Y'
-#     }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [( 
+        'rest_framework.authentication.SessionAuthentication' 
+        if 'DEV' in os.environ  
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    )], 
+    'DEFAULT_PAGINATION_CLASS':  'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DATETIME_FORMAT': '%d %b %Y'
+    }
 
-# if 'DEV' not in os.environ:
-#     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
-#         'rest_framework.renderers.JSONRenderer'
-#     ]
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer'
+    ]
 
-# REST_USE_JWT = True
+REST_USE_JWT = True
 
-# JWT_AUTH_COOKIE = 'my-app-auth'
+JWT_AUTH_COOKIE = 'my-app-auth'
 
-# JWT_AUTH_SECURE = True
-# JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
-# REST_AUTH_SERIALIZERS = {'USER_DETAILS_SERIALIZER': 'drf_app.serializers.CurrentUserSerializer'}
+JWT_AUTH_SECURE = True
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+REST_AUTH_SERIALIZERS = {'USER_DETAILS_SERIALIZER': 'drf_app.serializers.CurrentUserSerializer'}
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
